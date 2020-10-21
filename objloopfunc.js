53e84f1aba1dd12b2d49d1c5469b238ad659152a
Jenny <finej01@gmail.com>
2020-10-21 15:40:48 -0400

    Add code

---
+++ objloopfunc.js
@@ -0,0 +1,11 @@
+const mandalorian = {
+    image: 'path/image',
+    logo: 'path/logo',
+    length: '1:00',
+    title: 'Special Look | The Madalorian | Disney +',
+    author: 'Star Wars',
+    views: 1000000000,
+    date: '1 day ago'
+}
+
+console.log(mandalorian.views)
